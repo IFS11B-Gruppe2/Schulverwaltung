225230e62d9f44f771b2accd009feb76ca75874a
Sergio Lindo <sergiolindo.empresa@gmail.com>
2015-07-13 11:35:15 +0200

    Create config.php and add database config var

---
+++ config/config.php
@@ -0,0 +1,16 @@
+<?php
+
+$database = array(
+	'host' => '',
+	'database' => '',
+	'users' => aray(
+		'mitarbeiter' => array(
+			'username' => '',
+			'password' => ''
+		),
+		'systembetreuer' => array(
+			'username' => '',
+			'password' => ''
+		)
+	)
+);
