cb373357bee330c84e107d29abe1ebd5e0a33d18
Sergio Lindo <sergiolindo.empresa@gmail.com>
2015-07-14 23:01:11 +0200

    Add personaldataMenu controller and view

---
+++ core/controllers/personaldata/personaldataMenuController.php
@@ -0,0 +1,9 @@
+<?php
+
+$rootPath = '../../..';
+
+$view = array(
+	'rootPath' => $rootPath
+);
+
+require_once($rootPath . '/views/personaldata/personalDataMenuView.php');
---
+++ views/personaldata/personalDataMenuView.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+	<meta charset="UTF-8">
+	<title>Stammdaten</title>
+
+	<base href="http://localhost/Schulverwaltung/" />
+
+	<link rel="stylesheet" type="text/css" href="css/libs/normalize-3.0.2.css" />
+</head>
+<body>
+
+<?php require_once($view['rootPath'] . '/views/menu.php'); ?>
+
+<nav>
+	<a href="core/controllers/personaldata/users/usersListController.php"> Benutzer </a>
+	<a href="core/controllers/personaldata/suppliers/suppliersListController.php"> Lieferanten </a>
+</nav>
+
+</body>
+</html>
