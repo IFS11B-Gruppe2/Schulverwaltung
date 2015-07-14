bde1b1471fd6b4d7121a73976c1c06386c67050a
Sergio Lindo <sergiolindo.empresa@gmail.com>
2015-07-15 00:09:26 +0200

    add css style to main menu

--- views/menu.php
+++ views/menu.php
@@ -1,7 +1,27 @@
-<nav>
-	<a href="core/controllers/rooms/roomsListController.php">Räume</a>
-	<a href="core/controllers/maincomponents/maincomponentsListController.php">Hauptkomponenten</a>
-	<a href="core/controllers/subcomponents/subcomponentsListController.php">Teilkomponenten</a>
-	<a href="core/controllers/personaldata/personaldataMenuController.php">Stammdaten</a>
-	<a href="core/controllers/logout/logout.php">Logout</a>
+<?php
+$tmp = explode('/', $_SERVER['SCRIPT_FILENAME']);
+$script_filename = $tmp[count($tmp) - 1];
+
+switch ($script_filename):
+  case 'roomsListController.php':
+		$roomsSelected = 'selected';
+		break;
+	case 'maincomponentsListController.php':
+		$maincomponentsSelected = 'selected';
+		break;
+	case 'subcomponentsListController.php':
+		$subcomponentsSelected = 'selected';
+		break;
+	case 'personaldataListController.php':
+		$personaldataSelected = 'selected';
+		break;
+endswitch;
+?>
+
+<nav class="mainMenu">
+	<a class="blueButton <?= $roomsSelected ?>" href="core/controllers/rooms/roomsListController.php"> Räume </a>
+	<a class="blueButton <?= $maincomponentsSelected ?>" href="core/controllers/maincomponents/maincomponentsListController.php"> Hauptkomponenten </a>
+	<a class="blueButton <?= $subcomponentsSelected ?>" href="core/controllers/subcomponents/subcomponentsListController.php"> Teilkomponenten </a>
+	<a class="blueButton <?= $personaldataSelected ?>" href="core/controllers/personaldata/personaldataMenuController.php"> Stammdaten </a>
+	<a class="redButton" href="core/controllers/logout/logout.php"> Logout </a>
 </nav>
