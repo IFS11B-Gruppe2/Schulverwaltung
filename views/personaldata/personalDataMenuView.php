99145347f31c66ad17bc8e2b3a9870de17b92c6e
Sergio Lindo <sergiolindo.empresa@gmail.com>
2015-07-15 20:53:44 +0200

    fix css style from personalDataMenuView

--- views/personaldata/personalDataMenuView.php
+++ views/personaldata/personalDataMenuView.php
@@ -7,14 +7,17 @@
 	<base href="<?= $CONFIG['webHost'] ?>/" />
 
 	<link rel="stylesheet" type="text/css" href="css/libs/normalize-3.0.2.css" />
+	<link rel="stylesheet" type="text/css" href="css/custom/classes.css" />
 </head>
 <body>
 
 <?php require_once($view['rootPath'] . '/views/menu.php'); ?>
 
 <nav>
-	<a href="core/controllers/personaldata/users/usersListController.php"> Benutzer </a>
-	<a href="core/controllers/personaldata/suppliers/suppliersListController.php"> Lieferanten </a>
+	<ul>
+		<li> <a href="core/controllers/personaldata/users/usersListController.php"> Benutzer </a> </li>
+		<li> <a href="core/controllers/personaldata/suppliers/suppliersListController.php"> Lieferanten </a> </li>
+	</ul>
 </nav>
 
 </body>
