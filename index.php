dfd40b2263e529f787f063ee2dd1d20940c96784
Sergio Lindo <sergiolindo.empresa@gmail.com>
2015-07-15 12:18:45 +0200

    fix index.php default controller

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
 
-header("Location: http://localhost/core/controllers/rooms/roomsListController.php");
+require_once('config/config.php');
+
+header("Location: $webProtocol$webHost/core/controllers/rooms/roomsListController.php");
 die('Error: Redirection has not worked.');
